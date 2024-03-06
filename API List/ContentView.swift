dbfc6628f33a4562b0e903ceba70555fbecf4d69
Keila Islas <kislas@bsd220.org>
2024-03-06 09:42:32 -0600

    Setup ContentView

--- API List/ContentView.swift
+++ API List/ContentView.swift
@@ -8,15 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var categories = [String]()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List(categories, id: \.self) { category in
+                NavigationLink(destination: Text(category)) {
+                    Text(category)
+                }
+            }
+            .navigationTitle("API Categories")
         }
-        .padding()
+        .task {
+            await getCategories()
+        }
     }
+    func getCategories() async {
+            let query = "https://api.publicapis.org/categories"
+            if let url = URL(string: query) {
+                if let (data, _) = try? await URLSession.shared.data(from: url) {
+                    if let decodedResponse = try? JSONDecoder().decode(Categories.self, from: data) {
+                        categories = decodedResponse.categories
+                    }
+                }
+            }
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -24,3 +39,9 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+struct Categories: Codable {
+    var categories: [String]
+}
+
